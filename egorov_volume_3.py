8774fc4dd1e988d0cc73764c24d0e83a19d51730
KirillEgorov1984 <49128258+KirillEgorov1984@users.noreply.github.com>
2025-01-22 14:14:24 +0200

    + lesson 3

---
+++ egorov_volume_3.py
@@ -0,0 +1,21 @@
+number = int(input('Введите число: '))
+
+# if number == 1:
+#     print('Один')
+# else:
+#     if number == 2:
+#         print('Два')
+#     else:
+#         if number == 3:
+#             print('Три')
+#         else:
+#             print('Неизвестное число')
+
+if number == 1:
+    print('Один')
+elif number == 2:
+    print('Два')
+elif number == 3:
+    print('Три')
+else:
+    print('Неизвестное число')
