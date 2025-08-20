3e72e85771dc28c68d7ae24a3b00ff485447242e
KirillEgorov1984 <49128258+KirillEgorov1984@users.noreply.github.com>
2025-08-20 16:41:28 +0300

    Lesson 5 5_8, 5_10

---
+++ Глава_5_Функции/5_10_изменяет_значение_параметров.py
@@ -0,0 +1,12 @@
+def main():
+    value = 99
+    print(f'Значение равно {value}.')
+    change_me(value)
+    print(f'После возвращения в функцию main значение равно {value}.')
+
+def change_me(arg):
+    print('Я изменяю значение.')
+    arg = 0
+    print(f'Теперь значение равно {arg}.')
+
+main()
---
+++ Глава_5_Функции/5_8_multiple_args.py
@@ -0,0 +1,10 @@
+def main():
+    print('Сумма чисел 12 и 45 равняется')
+    show_sum(12, 45) # аргументы
+
+
+def show_sum(num1, num2): # параметры
+    result = num1 + num2
+    print(result)
+
+main()
