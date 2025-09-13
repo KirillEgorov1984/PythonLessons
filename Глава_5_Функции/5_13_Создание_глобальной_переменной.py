a098084a106b9b6b76cac29a5347959087181f65
KirillEgorov1984 <49128258+KirillEgorov1984@users.noreply.github.com>
2025-09-13 12:50:27 +0300

    dsd
    create

---
+++ Глава_5_Функции/5_13_Создание_глобальной_переменной.py
@@ -0,0 +1,19 @@
+my_value =10
+
+def show_value():
+    print(my_value)
+show_value()
+
+
+# Пример объявления глобальной переменной
+number = 0
+
+def main():
+    global number
+    number = int(input('Введите число: '))
+    show_number()
+
+def show_number():
+    print(f'Вы ввели число {number}')
+
+main()
